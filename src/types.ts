bab92366e5ae477ca1eed381d2bacb82721ca971
Dan Ruan <danrsaquetti@gmail.com>
2025-12-12 13:59:41 -0300

    feat: Definindo tipagem das estruturas dos metadados

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type MetadataTarget = Function | Object
+export type MetadataKey = string | symbol
+export type MetadataValue = unknown
+
+export type PropertyKey = string | symbol
+
+export type MetadataMap = Map<MetadataKey, MetadataValue>
+
+export type ClassMetadataMap = MetadataMap
+export type PropertyMetadataStore = Map<PropertyKey, MetadataMap>
+export type MethodMetadataStore = Map<PropertyKey, MetadataMap>
+export type ParameterMetadataStore = Map<PropertyKey, Map<number, MetadataMap>>
+
+export interface MetadataContainer {
+  class: ClassMetadataMap
+  properties: PropertyMetadataStore
+  methods: MethodMetadataStore
+  parameters: ParameterMetadataStore
+}
