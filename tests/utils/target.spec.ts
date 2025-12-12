04c8efe925f5743999a4b8391a4b20402a25f3ad
Dan Ruan <danrsaquetti@gmail.com>
2025-12-12 15:24:54 -0300

    test: Criando caso de teste que valida se os variados valores são definidos como targets válidos

--- tests/utils/target.spec.ts
+++ tests/utils/target.spec.ts
@@ -2,7 +2,7 @@ import { test, describe, expect, beforeEach } from 'vitest'
 
 import { ReflectMetadata } from '@reflect-metadata'
 import { isValidTarget } from '@utils/target'
-import { INVALID_TARGET_LIST } from '../common/invalid-target-list'
+import { INVALID_TARGET_LIST, VALID_TARGET_LIST } from '../common/types-target-list'
 
 describe('Target inválidos', () => {
   let reflect: ReflectMetadata
@@ -12,10 +12,17 @@ describe('Target inválidos', () => {
     reflect = new ReflectMetadata()
   })
 
-  test('Esperado disparar uma exceção quando tentar registrar um metadado com um target inválido', () => {
+  test('Esperado que diferentes tipos de dados sejam classificados como targets inválidos', () => {
     INVALID_TARGET_LIST.forEach(value => {
       expect(isValidTarget(value), `expected "${String(value)}" (${typeof value}) to be a invalid target`)
         .toBe(false)
+    })
+  })
+
+  test('Esperado que diferentes tipos de dados sejam classificados como targets válidos', () => {
+    VALID_TARGET_LIST.forEach(value => {
+      expect(isValidTarget(value), `expected "${String(value)}" (${typeof value}) to be a valid target`)
+        .toBe(true)
     })
   })
 })
