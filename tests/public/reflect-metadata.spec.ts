727fe31415c9c092ec856bbfbdca187f0ce3057e
Dan Ruan <danrsaquetti@gmail.com>
2026-01-05 15:47:32 -0300

    test: Criando casos de teste para validação da escrita e leitura da API da classe `ReflectMetadata`

---
+++ tests/public/reflect-metadata.spec.ts
@@ -0,0 +1,104 @@
+import { beforeEach, describe, expect, test } from 'vitest'
+
+import { ReflectMetadata } from '@public/reflect-metadata'
+
+describe('ReflectMetadata public API', () => {
+  let reflect: ReflectMetadata
+
+  beforeEach(() => {
+    reflect = new ReflectMetadata()
+  })
+
+  test('class metadata: define/get/has/delete', () => {
+    class ClassWithMetadata { }
+
+    reflect.defineMetadata('key', 'value', ClassWithMetadata)
+    expect(reflect.hasMetadata('key', ClassWithMetadata)).toBe(true)
+    expect(reflect.getMetadata('key', ClassWithMetadata)).toBe('value')
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadata)).toBe(true)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadata)).toBe('value')
+
+    reflect.deleteMetadata('key', ClassWithMetadata)
+    expect(reflect.hasMetadata('key', ClassWithMetadata)).toBe(false)
+    expect(reflect.getMetadata('key', ClassWithMetadata)).toBeUndefined()
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadata)).toBe(false)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadata)).toBeUndefined()
+  })
+
+  test('property metadata: define/get/has/delete', () => {
+    class ClassWithMetadataInProperty {
+      prop: any
+    }
+
+    reflect.defineMetadata('key', 'value', ClassWithMetadataInProperty.prototype, 'prop')
+    expect(reflect.hasMetadata('key', ClassWithMetadataInProperty.prototype, 'prop')).toBe(true)
+    expect(reflect.getMetadata('key', ClassWithMetadataInProperty.prototype, 'prop')).toBe('value')
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadataInProperty.prototype, 'prop')).toBe(true)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadataInProperty.prototype, 'prop')).toBe('value')
+
+    reflect.deleteMetadata('key', ClassWithMetadataInProperty.prototype, 'prop')
+    expect(reflect.hasMetadata('key', ClassWithMetadataInProperty.prototype, 'prop')).toBe(false)
+    expect(reflect.getMetadata('key', ClassWithMetadataInProperty.prototype, 'prop')).toBeUndefined()
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadataInProperty.prototype, 'prop')).toBe(false)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadataInProperty.prototype, 'prop')).toBeUndefined()
+  })
+
+  test('method metadata: define/get/has/delete', () => {
+    class ClassWithMetadataInMethod {
+      method() { }
+    }
+
+    reflect.defineMetadata('key', 'value', ClassWithMetadataInMethod.prototype, 'method')
+    expect(reflect.hasMetadata('key', ClassWithMetadataInMethod.prototype, 'method')).toBe(true)
+    expect(reflect.getMetadata('key', ClassWithMetadataInMethod.prototype, 'method')).toBe('value')
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadataInMethod.prototype, 'method')).toBe(true)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadataInMethod.prototype, 'method')).toBe('value')
+
+    reflect.deleteMetadata('key', ClassWithMetadataInMethod.prototype, 'method')
+    expect(reflect.hasMetadata('key', ClassWithMetadataInMethod.prototype, 'method')).toBe(false)
+    expect(reflect.getMetadata('key', ClassWithMetadataInMethod.prototype, 'method')).toBeUndefined()
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadataInMethod.prototype, 'method')).toBe(false)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadataInMethod.prototype, 'method')).toBeUndefined()
+  })
+
+  test('parameter metadata: define/get/has/delete', () => {
+    class ClassWithMetadataInParameter {
+      method(a: any, b: any) { }
+    }
+
+    reflect.defineMetadata('key', 'value', ClassWithMetadataInParameter.prototype, 'method', 0)
+    reflect.defineMetadata('key', 'another-value', ClassWithMetadataInParameter.prototype, 'method', 1)
+
+    expect(reflect.hasMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 0)).toBe(true)
+    expect(reflect.getMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 0)).toBe('value')
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 0)).toBe(true)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 0)).toBe('value')
+    expect(reflect.hasMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 1)).toBe(true)
+    expect(reflect.getMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 1)).toBe('another-value')
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 1)).toBe(true)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 1)).toBe('another-value')
+
+    reflect.deleteMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 0)
+    expect(reflect.hasMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 0)).toBe(false)
+    expect(reflect.getMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 0)).toBeUndefined()
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 0)).toBe(false)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 0)).toBeUndefined()
+    expect(reflect.hasMetadata('key', ClassWithMetadataInParameter.prototype, 'method', 1)).toBe(true)
+  })
+
+  test('constructor metadata: define/get/has/delete on class', () => {
+    class ClassWithMetadataInConstructor { constructor(a: any) { } }
+
+    reflect.defineMetadata('key', 'value', ClassWithMetadataInConstructor, 'constructor', 0)
+    expect(reflect.hasMetadata('key', ClassWithMetadataInConstructor, 'constructor', 0)).toBe(true)
+    expect(reflect.getMetadata('key', ClassWithMetadataInConstructor, 'constructor', 0)).toBe('value')
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadataInConstructor, 'constructor', 0)).toBe(true)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadataInConstructor, 'constructor', 0)).toBe('value')
+
+    reflect.deleteMetadata('key', ClassWithMetadataInConstructor, 'constructor', 0)
+    expect(reflect.hasMetadata('key', ClassWithMetadataInConstructor, 'constructor', 0)).toBe(false)
+    expect(reflect.getMetadata('key', ClassWithMetadataInConstructor, 'constructor', 0)).toBeUndefined()
+    expect(reflect.hasOwnMetadata('key', ClassWithMetadataInConstructor, 'constructor', 0)).toBe(false)
+    expect(reflect.getOwnMetadata('key', ClassWithMetadataInConstructor, 'constructor', 0)).toBeUndefined()
+  })
+})
